4c4e1983d548a48999f252d586bbc23ebb18865f
RoyPrinsGH <royprinsgm@gmail.com>
2025-06-25 22:38:56 +0200

    Refactor section adding logic into separate function

--- miniparse-lib/src/lib.rs
+++ miniparse-lib/src/lib.rs
@@ -21,6 +21,23 @@ pub enum ParseError {
     RegexCaptureGroupNameMismatch(&'static str),
 }
 
+fn add_section_to_ini_builder<'content>(
+    ini_file_builder: IniFileBuilder<'content>,
+    current_section_builder: IniSectionBuilder<'content>,
+) -> IniFileBuilder<'content> {
+    let (id, section) = current_section_builder.build();
+
+    log::debug!("Adding section {id:?}: {section:?}");
+
+    // Do not add global section if it is empty. We can do this with named sections, because their start is explicit
+    // but global section definitions are implicit.
+    match id {
+        SectionId::Global if !section.entries.is_empty() => ini_file_builder.set_global_section(section),
+        SectionId::Named(name) => ini_file_builder.new_section(name, section),
+        _ => ini_file_builder,
+    }
+}
+
 pub fn parse<'content>(ini_string: &'content str) -> Result<IniFile<'content>, ParseError> {
     let key_value_regex = Regex::new(&format!(
         r"^\s*(?P<{ENTRY_KEY_GROUP_NAME}>[^=\s]+)\s*=\s*(?P<{ENTRY_VALUE_GROUP_NAME}>[^=\s]+)\s*$"
@@ -45,25 +62,15 @@ pub fn parse<'content>(ini_string: &'content str) -> Result<IniFile<'content>, P
         }
 
         if let Some(section_header_captures) = section_header_regex.captures(line) {
-            log::debug!("Line matched section start regex");
+            log::debug!("Line matched section start regex, adding current section");
+            ini_file_builder = add_section_to_ini_builder(ini_file_builder, current_section_builder);
 
-            let (id, section) = current_section_builder.build();
+            let new_section_name = section_header_captures
+                .name(SECTION_NAME_GROUP_NAME)
+                .ok_or(ParseError::RegexCaptureGroupNotFound(SECTION_NAME_GROUP_NAME))?
+                .as_str();
 
-            log::debug!("Adding section {id:?}: {section:?}");
-
-            ini_file_builder = match id {
-                // Do not add global section if it is empty. We can do this with named sections, because their start is explicit
-                // but global section definitions are implicit.
-                SectionId::Global if !section.entries.is_empty() => ini_file_builder.set_global_section(section),
-                SectionId::Named(name) => ini_file_builder.new_section(name, section),
-                _ => ini_file_builder,
-            };
-
-            match section_header_captures.name(SECTION_NAME_GROUP_NAME) {
-                Some(section_name) => current_section_builder = IniSectionBuilder::new().set_id(SectionId::Named(section_name.as_str())),
-                None => return Err(ParseError::RegexCaptureGroupNotFound(SECTION_NAME_GROUP_NAME)),
-            }
-
+            current_section_builder = IniSectionBuilder::new().set_id(SectionId::Named(new_section_name));
             continue;
         }
 
@@ -71,19 +78,9 @@ pub fn parse<'content>(ini_string: &'content str) -> Result<IniFile<'content>, P
     }
 
     log::debug!("End of file reached. Adding current section, if we are building one.");
-
-    let (id, section) = current_section_builder.build();
-
-    log::debug!("Adding section {id:?}: {section:?}");
-
-    ini_file_builder = match id {
-        SectionId::Global if !section.entries.is_empty() => ini_file_builder.set_global_section(section),
-        SectionId::Named(name) => ini_file_builder.new_section(name, section),
-        _ => ini_file_builder,
-    };
+    ini_file_builder = add_section_to_ini_builder(ini_file_builder, current_section_builder);
 
     log::debug!("Building ini file");
-
     Ok(ini_file_builder.build())
 }
 
